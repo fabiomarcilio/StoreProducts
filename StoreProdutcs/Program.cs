6865794f7cb795bf8de71a14bfdeb0613fdb895c
Fabio Marcílio <fabiomarcilio@msn.com>
2020-05-18 18:25:28 -0300

    Programa exemplo herança e polimorfismo

---
+++ StoreProdutcs/Program.cs
@@ -0,0 +1,60 @@
+﻿using StoreProdutcs.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+
+namespace StoreProdutcs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            Console.Write("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++ )
+            {
+                Console.WriteLine($"Product #{i} data:");
+                Console.Write("Common, used or imported (c/u/i)? ");
+                string choice = Console.ReadLine();
+                if (choice == "c")
+                {
+                    Console.Write("Name: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Price: ");
+                    double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new Product(name, price));
+                }
+                else if (choice == "u")
+                {
+                    Console.Write("Name: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Price: ");
+                    double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    Console.Write("Manufacture date (DD/MM/YYYY): ");
+                    DateTime date = DateTime.Parse(Console.ReadLine());
+                    list.Add(new UsedProduct(name, price, date));
+                }
+                else if (choice == "i")
+                {
+                    Console.Write("Name: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Price: ");
+                    double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    Console.Write("Customs fee: ");
+                    double fee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new ImportedProduct(name, price, fee));
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("PRICE TAGS:");
+            foreach(Product prod in list)
+            {
+                Console.WriteLine(prod.PriceTag());
+            }
+
+        }
+    }
+}
